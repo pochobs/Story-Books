351edd559be8e3850ba60884251c65c7048a18cb
Pochobs <pochobs@hotmail.com>
2020-09-25 21:48:56 -0500

    Video min 1:02:43 , all working

--- config/passport.js
+++ config/passport.js
@@ -15,7 +15,7 @@ module.exports = function(passport) {
             displayName: profile.displayName,
             firstName: profile.name.givenName,
             lastName: profile.name.familyName,
-            image: profile.photos[0].value,
+            image: profile.photos[0].value
         }
         try {
             let user = await User.findOne({ googleId: profile.id})
